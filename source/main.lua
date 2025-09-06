584fafcd1daf02a7328ac6564cb7a662bbf2709d
Craig P. Motlin <cmotlin@gmail.com>
2025-09-06 13:31:05 -0400

    Add collision detection and score tracking.

--- source/main.lua
+++ source/main.lua
@@ -18,12 +18,14 @@ playerSprite:add()
 
 -- Game State
 local gameState = "stopped" -- Possible states: "stopped", "active"
+local score = 0
 
 -- Obstacle
 local obstacleSpeed = 5
 local obstacleImage = gfx.image.new("images/capybara")
 assert(obstacleImage, "Failed to load obstacle image")
 local obstacleSprite = gfx.sprite.new(obstacleImage)
+obstacleSprite.collisionResponse = gfx.sprite.kCollisionTypeOverlap
 obstacleSprite:setCollideRect(0, 0, obstacleSprite:getSize())
 obstacleSprite:setImageFlip(gfx.kImageFlippedX)
 obstacleSprite:moveTo(450, 240)
@@ -31,10 +33,12 @@ obstacleSprite:add()
 
 function pd.update()
     gfx.sprite.update()
+
     if gameState == "stopped" then
         gfx.drawTextAligned("Press A to Start", 200, 40, kTextAlignment.center)
         if pd.buttonJustPressed(pd.kButtonA) then
             gameState = "active"
+            score = 0
             playerSprite:moveTo(playerStartX, playerStartY)
             obstacleSprite:moveTo(450, math.random(40, 200))
         end
@@ -43,17 +47,20 @@ function pd.update()
         local crankRadians = math.rad(crankPosition)
         local verticalSpeed = -math.cos(crankRadians) * playerSpeed
         playerSprite:moveBy(0, verticalSpeed)
-        obstacleSprite:moveBy(-obstacleSpeed, 0)
+        local obstacleX, obstacleY, collisions, collisionCount =
+            obstacleSprite:moveWithCollisions(obstacleSprite.x - obstacleSpeed, obstacleSprite.y)
 
         -- Reset obstacle position if it goes off screen
         if obstacleSprite.x < -20 then
             obstacleSprite:moveTo(450, math.random(40, 200))
+            score += 1
+        end
+
+        -- Keep player within screen bounds
+        if collisionCount > 0 or playerSprite.y > 270 or playerSprite.y < -30 then
+            gameState = "stopped"
         end
     end
 
-    -- Keep player within screen bounds
-    if playerSprite.y > 270 or playerSprite.y < -30 then
-        gameState = "stopped"
-    end
-
+    gfx.drawTextAligned("Score: " .. score, 390, 10, kTextAlignment.right)
 end
