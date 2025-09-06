69b4c1e46291ad67e3d3b5f3b220f7157cfdf4cf
Craig P. Motlin <cmotlin@gmail.com>
2025-09-06 10:58:13 -0400

    Add game states and boundary checking.

--- source/main.lua
+++ source/main.lua
@@ -1,3 +1,4 @@
+import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 
 local pd = playdate
@@ -15,10 +16,27 @@ playerSprite:setCollideRect(4, 4, width - 8, height - 8)
 playerSprite:moveTo(playerStartX, playerStartY)
 playerSprite:add()
 
+-- Game State
+local gameState = "stopped" -- Possible states: "stopped", "active"
+
 function pd.update()
-    local crankPosition = pd.getCrankPosition()
-    local crankRadians = math.rad(crankPosition)
-    local verticalMovement = -math.cos(crankRadians) * playerSpeed
-    playerSprite:moveBy(0, verticalMovement)
     gfx.sprite.update()
+    if gameState == "stopped" then
+        gfx.drawTextAligned("Press A to Start", 200, 40, kTextAlignment.center)
+        if pd.buttonJustPressed(pd.kButtonA) then
+            gameState = "active"
+            playerSprite:moveTo(playerStartX, playerStartY)
+        end
+    elseif gameState == "active" then
+        local crankPosition = pd.getCrankPosition()
+        local crankRadians = math.rad(crankPosition)
+        local verticalSpeed = -math.cos(crankRadians) * playerSpeed
+        playerSprite:moveBy(0, verticalSpeed)
+    end
+
+    -- Keep player within screen bounds
+    if playerSprite.y > 270 or playerSprite.y < -30 then
+        gameState = "stopped"
+    end
+
 end
